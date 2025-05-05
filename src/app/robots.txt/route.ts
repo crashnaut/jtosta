ff866df62e6350dca8f15da692d5432901656991
Mike Sell <mikesell404@gmail.com>
2025-05-05 15:48:07 +0000

    Initial commit

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://jstosta.com' // Replace with actual domain later
+  const baseUrl = 'https://jtosta.com' 
 
   return {
     rules: {
