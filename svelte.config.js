185655743ef3c1e19f79a72ac27bcdc4857dd2d3
Crashnaut <crashnaut@protonmail.com>
2025-05-08 19:00:45 -0300

    Configure Cloud Run deployment with SvelteKit SSR

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import rehypeUnwrapImages from 'rehype-unwrap-images';
@@ -7,13 +7,7 @@ import rehypeUnwrapImages from 'rehype-unwrap-images';
 const config = {
     extensions: ['.svelte', '.md'],
     kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: 'index.html',
-            precompress: false,
-            strict: true
-        }),
+        adapter: adapter(),
         alias: {
             $lib: './src/lib'
         },
